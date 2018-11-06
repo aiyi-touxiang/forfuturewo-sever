20eed9a39dfea2bf72d484270b128b6322dfc9f5
胡伟健 <598307691@qq.com>
2018-11-06 15:46:59 +0800

    初始化server

---
+++ app/controller/list.js
@@ -0,0 +1,22 @@
+/**
+ * author: huweijian
+ * Date: 2018/11/6 - 3:11 PM
+ * Name: lists
+ * Desc:
+ */
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class ListController extends Controller {
+  async name () {
+    this.ctx.body = [
+      {
+        id: 1,
+        name: '张三'
+      }
+    ];
+  }
+}
+
+module.exports = ListController;
